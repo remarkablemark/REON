791e87f2a79ab091bc9d64650181d645ae174063
Mark <mark@remarkablemark.org>
2016-09-01 21:01:54 -0400

    Write tests for `parse` method
    Check error handling and that `parse` converts a JSON string
    that is shaped like a React element into a valid React element.

--- test/index.js
+++ test/index.js
@@ -24,7 +24,7 @@ describe('REON', function() {
             });
         });
 
-        it('converts React element object to string', function() {
+        it('converts React element to string', function() {
             assert.deepEqual(
                 REON.stringify(React.createElement('div', {
                     className: 'foo'
@@ -40,6 +40,93 @@ describe('REON', function() {
                     _owner: null,
                     _store: {}
                 })
+            );
+        });
+
+    });
+
+    /**
+     * Parse.
+     */
+    describe('#parse', function() {
+
+        it('throws an error if argument is invalid', function() {
+            var values = [
+                undefined, null, {}, [], 42, { type: null },
+                '""', 'null', '42', 'foo', '"foo"'
+            ];
+            values.forEach(function(value) {
+                assert.throws(function() { REON.parse(value); });
+            });
+        });
+
+        it('converts a valid string to React elements', function() {
+            var parsed = REON.parse(
+                JSON.stringify({
+                    type: 'div',
+                    props: {
+                        className: 'foo',
+                        children: 'text'
+                    }
+                })
+            );
+
+            assert(React.isValidElement(parsed));
+            assert.deepEqual(
+                parsed,
+                React.createElement('div', {
+                    className: 'foo'
+                }, 'text')
+            );
+        });
+
+        it('converts a complex string to React elements', function() {
+            var parsed = REON.parse(
+                JSON.stringify({
+                    type: 'div',
+                    key: null,
+                    props: {
+                        foo: 'foo',
+                        children: [
+                            {
+                                type: 'p',
+                                key: 1,
+                                props: { children: 'bar' }
+                            },
+                            ['baz'],
+                            'text'
+                        ]
+                    }
+                })
+            );
+
+            assert(React.isValidElement(parsed));
+            assert.deepEqual(
+                parsed,
+                React.createElement('div', { foo: 'foo' },
+                    React.createElement('p', { key: 1 }, 'bar'),
+                    ['baz'], 'text'
+                )
+            );
+        });
+
+        it('converts stringified React element back to valid React element', function() {
+            var reactElement = React.createElement('body', {},
+                React.createElement('h1', { style: { color: '#f00' } }, 'foo'),
+                'bar',
+                React.createElement('ul', {},
+                    React.createElement('li', {}, 'one'),
+                    React.createElement('li', {}, 'two')
+                )
+            );
+            var parsed = REON.parse(
+                REON.stringify(reactElement)
+            );
+
+            assert(React.isValidElement(parsed));
+            assert.deepEqual(
+                REON.parse(REON.stringify(reactElement)),
+                reactElement
             );
         });
 
